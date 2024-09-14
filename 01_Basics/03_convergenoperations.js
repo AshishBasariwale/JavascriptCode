aff39b1cc333cac43171e94a75309b4a7b476083
AshishBasariwale <148336309+AshishBasariwale@users.noreply.github.com>
2024-09-14 08:24:17 +0000

    comparision

--- 01_Basics/03_convergenoperations.js
+++ 01_Basics/03_convergenoperations.js
@@ -1,32 +1,77 @@
-let score =  true;
+// let score =  true;
 
 
-//console.log(typeof score);
-//console.log(typeof (score));
+// //console.log(typeof score);
+// //console.log(typeof (score));
 
 
-let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// //console.log(typeof valueInNumber);
 
-//console.log(valueInNumber);
+// //console.log(valueInNumber);
 
 
-// "33" =>33
-// "33abc" =>NaN
-// "true"=> 1; false => 0;
+// // "33" =>33
+// // "33abc" =>NaN
+// // "true"=> 1; false => 0;
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLogIn = Boolean(isLoggedIn);
+// let booleanIsLogIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLogIn)
+// console.log(booleanIsLogIn)
 
-// 1 => true ; 0 => false;
-// ""=>false
-//"ashis" => true
+// // 1 => true ; 0 => false;
+// // ""=>false
+// //"ashis" => true
 
-let SomeNumber = 44
+// let SomeNumber = 44
 
-let Stringnuber = String(SomeNumber);
+// let Stringnuber = String(SomeNumber);
 
-console.log(typeof Stringnuber)
+// console.log(typeof Stringnuber)
+
+
+// operations///
+
+let value = 3;
+let negValue = -value
+
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // 2 rest to power 2 
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " ashish"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 , num2 , num3 
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
