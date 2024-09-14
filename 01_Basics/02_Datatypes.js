d1030041a3bcf5dbe84d53b14835db64373c5bd0
AshishBasariwale <148336309+AshishBasariwale@users.noreply.github.com>
2024-09-14 07:23:44 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,28 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3)// we are using nodejs , not browser
+
+console.log(
+    3+3); // code readiability should be high
+
+console.log("ashish")
+
+let name = "ashish"
+let age = 18;
+let isLoggedIn = true;
+
+//premitive 
+//number =>2 to the power of 53
+//bigint => if number is big 
+//string => ""
+//boolean => true/false
+//null => standalone value 
+//undefiined 
+//symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // bject 
+
