66ed023dd12b550dd85dd760fa6f7412dfe8276f
AshishBasariwale <148336309+AshishBasariwale@users.noreply.github.com>
2024-09-14 09:30:39 +0000

    datatypesummery

---
+++ 01_Basics/datatypes-summery.js
@@ -0,0 +1,39 @@
+// primitive data type 
+//7 types 
+//string,number,boolean,null(empty),undefiend,Symbol 
+
+// Non - primitive
+// Array , objects , functions 
+
+const score   =100;
+
+const scoreValue = 100.3;
+
+const isLoggdIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId = Sysmbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 332332323232323n
+
+
+// array , objects , functions 
+
+const heros = ["shaktiman","naargraj","doga "]
+
+let myObj = {
+    name: "Ashish",
+    age : 24
+
+}
+
+const myFunction = function()
+{
+    console.log("hello inside the function ");
+}
+
